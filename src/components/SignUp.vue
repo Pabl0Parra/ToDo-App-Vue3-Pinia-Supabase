e401b95b8df044f5dc0c318512e2b62566c5fade
Pabl0Parra <pabsblue16@gmail.com>
2022-10-04 13:45:47 +0200

    sign up  styles component 80% done

--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -1,8 +1,8 @@
 <template>
-  <div>
+  <div class="bg-black">
     <h1 class="text-3xl font-bold underline">Sign Up</h1>
 
-    <p>Create your personal account to manage your own tasks.</p>
+    <p></p>
 
     <div class="max-w-screen-sm mx-auto px-4 py-10">
       <!-- Error Handling -->
@@ -13,9 +13,11 @@
       <!-- Register -->
       <form
         @submit.prevent="signUp()"
-        class="p-8 flex flex-col bg-light-grey rounded-md shadow-lg"
+        class="bg-gradient-to-r from-orange-400 to-orange-800 p-8 flex flex-col bg-light-grey rounded-md shadow-lg"
       >
-        <h1 class="text-3xl text-at-light-green mb-4">Register</h1>
+        <h1 class="text-3xl text-center font-bold text-black-500 mb-2">
+          CREATE YOUR ACCOUNT
+        </h1>
 
         <div class="flex flex-col mb-2">
           <label for="email" class="mb-1 text-sm text-at-light-green"
@@ -58,7 +60,7 @@
 
         <button
           type="submit"
-          class="mt-6 py-2 px-6 rounded-sm self-start text-sm text-white bg-at-light-green duration-200 border-solid border-2 border-transparent hover:border-at-light-green hover:bg-white hover:text-at-light-green"
+          class="mt-6 py-2 px-6 self-start text-sm text-white bg-black duration-200 border-solid border-2 border-transparent hover:border-at-light-green hover:bg-yellow-400 hover:text-black rounded"
         >
           Sign Up
         </button>
@@ -66,13 +68,17 @@
           <span class="text-sm mt-6 text-center"
             >Already have an account?
           </span>
-          <PersonalRouter :route="route" :buttonText="buttonText" />
+          <PersonalRouter
+            :route="route"
+            :buttonText="buttonText"
+            class="hover:bg-yellow-500 py-2 px-6 rounded"
+          />
         </p>
       </form>
     </div>
   </div>
 </template>
-<PersonalRouter :route="route" :buttonText="buttonText" />
+
 <script setup>
 import { ref, computed } from "vue";
 import { useRouter } from "vue-router";
