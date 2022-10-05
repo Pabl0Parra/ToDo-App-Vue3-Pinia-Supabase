dd256112ee742439b902dd362a4ae152aad61408
Pabl0Parra <pabsblue16@gmail.com>
2022-10-05 17:20:21 +0200

    implemented all functionalities for supabase inside task.js

--- src/stores/task.js
+++ src/stores/task.js
@@ -2,11 +2,13 @@ import { defineStore } from "pinia";
 import { supabase } from "../supabase";
 import { useUserStore } from "./user";
 
+// define task store and saved it into a constant for reference
 export const useTaskStore = defineStore("tasks", {
   state: () => ({
     tasks: null,
   }),
   actions: {
+    // async function to fetch all tasks from supabase
     async fetchTasks() {
       const { data: tasks } = await supabase
         .from("tasks")
@@ -15,7 +17,7 @@ export const useTaskStore = defineStore("tasks", {
       this.tasks = tasks;
       return this.tasks;
     },
-    // New code
+    // async function to add a task in supabase
     async addTask(title, description) {
       console.log(useUserStore().user.id);
       const { data, error } = await supabase.from("tasks").insert([
@@ -27,19 +29,41 @@ export const useTaskStore = defineStore("tasks", {
         },
       ]);
     },
-
-    async completeTask(taskID) {
+    // async function to mark a task as completed in supabase
+    async completeTask(id) {
       const { data, error } = await supabase
         .from("tasks")
         .update({ is_complete: true })
-        .eq("id", taskID);
+        .eq("id", id);
     },
-
+    // async function to mark a task as uncompleted in supabase
+    async uncompletedTask(id) {
+      const { data, error } = await supabase
+        .from("tasks")
+        .update({ is_complete: false })
+        .eq("id", id);
+    },
+    // async function to update a task in supabase
+    async modifyTask(id, newTitle, newDescription) {
+      const { data, error } = await supabase
+        .from("tasks")
+        .update({ title: newTitle, description: newDescription })
+        // equal to (to update only matching rows)
+        .eq("id", id);
+    },
+    // async function to delete all tasks from supabase
     async deleteAllTasks() {
       const { data, error } = await supabase
         .from("tasks")
         .delete()
         .eq("is_complete", false);
+    },
+    // async function to delete a particular task from supabase
+    async deleteCertainTask(id) {
+      const { data, error } = await supabase
+        .from("tasks")
+        .delete()
+        .eq("id", id);
     },
   },
 });
